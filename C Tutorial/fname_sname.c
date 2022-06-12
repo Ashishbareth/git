cfe702247f0638237c86e3f58a04b19384e1d9f1
AshishBareth <ashishbareth@gmail.com>
2022-06-12 21:19:51 +0530

    add this

---
+++ C Tutorial/fname_sname.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+   int a,b,sum=0;
+   printf("enter a no:");
+   scanf("%d",&a);
+   printf(" enter a no:");
+   scanf("%d",&b);
+   sum=a+b;
+   printf("the sum is :%d",sum);
+    return 0;
+}
